33d04f500337085ecded459e509b7abf8a831590
VodeniZeko <salexy.es@gmail.com>
2020-08-19 16:52:45 -0700

    player1

--- android/app/src/main/kotlin/gamers/playerone/player_one/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package gamers.playerone.player_one
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ player1/android/app/src/main/kotlin/gamers/playerone/player1/MainActivity.kt
@@ -0,0 +1,6 @@
+package gamers.playerone.player1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
